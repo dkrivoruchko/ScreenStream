6055ede894f4263387ae57ad3f4a7373286c05c5
Dmitriy Krivoruchko <dkrivoruchko@gmail.com>
2016-07-21 12:53:57 +0300

    Memory optimisation in ImageGenerator

--- app/src/main/java/info/dvkr/screenstream/ImageGenerator.java
+++ app/src/main/java/info/dvkr/screenstream/ImageGenerator.java
@@ -18,11 +18,10 @@ import com.google.firebase.crash.FirebaseCrash;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 
 final class ImageGenerator {
     private final Object lock = new Object();
-    private final ImageReader.OnImageAvailableListener imageListener;
+    private static final int VIRTUAL_DISPLAY_FLAGS = DisplayManager.VIRTUAL_DISPLAY_FLAG_OWN_CONTENT_ONLY | DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC;
 
     private volatile boolean isThreadRunning;
 
@@ -30,54 +29,63 @@ final class ImageGenerator {
     private Handler imageHandler;
     private ImageReader imageReader;
     private VirtualDisplay virtualDisplay;
+    private Bitmap reusableBitmap;
 
     private final String defaultText1;
     private final String defaultText2;
     private final String defaultText3;
 
+    private class ImageAvailableListener implements ImageReader.OnImageAvailableListener {
+        private Image image;
+        private Image.Plane plane;
+        private int width;
+
+        private Bitmap bitmapClean;
+        private byte[] jpegByteArray;
+
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            synchronized (lock) {
+                if (!isThreadRunning) return;
+
+                image = imageReader.acquireLatestImage();
+                if (image == null) return;
+
+                plane = image.getPlanes()[0];
+                width = plane.getRowStride() / plane.getPixelStride();
+
+                if (width > image.getWidth()) {
+                    if (reusableBitmap == null)
+                        reusableBitmap = Bitmap.createBitmap(width, image.getHeight(), Bitmap.Config.ARGB_8888);
+                    reusableBitmap.copyPixelsFromBuffer(plane.getBuffer());
+                    bitmapClean = Bitmap.createBitmap(reusableBitmap, 0, 0, image.getWidth(), image.getHeight());
+                } else {
+                    bitmapClean = Bitmap.createBitmap(width, image.getHeight(), Bitmap.Config.ARGB_8888);
+                    bitmapClean.copyPixelsFromBuffer(plane.getBuffer());
+                }
+
+                image.close();
+
+                try (final ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream(56)) {
+                    bitmapClean.compress(Bitmap.CompressFormat.JPEG, ApplicationContext.getApplicationSettings().getJpegQuality(), jpegOutputStream);
+                    jpegOutputStream.flush();
+                    jpegByteArray = jpegOutputStream.toByteArray();
+                } catch (IOException e) {
+                    FirebaseCrash.report(e);
+                }
+
+                bitmapClean.recycle();
+
+                if (jpegByteArray != null) ApplicationContext.getJPEGQueue().add(jpegByteArray);
+                jpegByteArray = null;
+            }
+        }
+    }
+
     ImageGenerator(final String defaultText1, final String defaultText2, final String defaultText3) {
         this.defaultText1 = defaultText1;
         this.defaultText2 = defaultText2;
         this.defaultText3 = defaultText3;
-
-        imageListener = new ImageReader.OnImageAvailableListener() {
-            @Override
-            public void onImageAvailable(ImageReader imageReader) {
-                synchronized (lock) {
-                    if (!isThreadRunning) return;
-
-                    final Image image = imageReader.acquireLatestImage();
-                    if (image == null) return;
-
-                    final Image.Plane[] planes = image.getPlanes();
-                    final ByteBuffer bytebuffer = planes[0].getBuffer();
-                    bytebuffer.rewind();
-                    final int width = planes[0].getRowStride() / planes[0].getPixelStride();
-
-                    Bitmap bitmap = Bitmap.createBitmap(width, image.getHeight(), Bitmap.Config.ARGB_8888);
-                    bitmap.copyPixelsFromBuffer(bytebuffer);
-
-                    if (width > image.getWidth()) {
-                        bitmap = Bitmap.createBitmap(bitmap, 0, 0, image.getWidth(), image.getHeight());
-                    }
-
-                    byte[] jpegByteArray;
-                    try (final ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream()) {
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, ApplicationContext.getApplicationSettings().getJpegQuality(), jpegOutputStream);
-                        jpegOutputStream.flush();
-                        jpegByteArray = jpegOutputStream.toByteArray();
-                    } catch (IOException e) {
-                        FirebaseCrash.report(e);
-                        return;
-                    }
-
-                    image.close();
-
-                    if (jpegByteArray != null) ApplicationContext.getJPEGQueue().add(jpegByteArray);
-                }
-            }
-        };
-
     }
 
     void start() {
@@ -88,15 +96,15 @@ final class ImageGenerator {
             imageThread.start();
             imageReader = ImageReader.newInstance(ApplicationContext.getScreenSize().x, ApplicationContext.getScreenSize().y, PixelFormat.RGBA_8888, 2);
             imageHandler = new Handler(imageThread.getLooper());
-            imageReader.setOnImageAvailableListener(imageListener, imageHandler);
+            imageReader.setOnImageAvailableListener(new ImageAvailableListener(), imageHandler);
             virtualDisplay = ApplicationContext.getMediaProjection().createVirtualDisplay(
                     "Screen Stream Virtual Display",
                     ApplicationContext.getScreenSize().x,
                     ApplicationContext.getScreenSize().y,
                     ApplicationContext.getScreenDensity(),
-                    DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                    VIRTUAL_DISPLAY_FLAGS,
                     imageReader.getSurface(),
-                    null, null);
+                    null, imageHandler);
 
             isThreadRunning = true;
 //            Log.d(TAG, "Image generator started.");
@@ -118,6 +126,11 @@ final class ImageGenerator {
             imageHandler.removeCallbacksAndMessages(null);
             imageThread.quit();
             imageThread = null;
+
+            if (reusableBitmap != null) {
+                reusableBitmap.recycle();
+                reusableBitmap = null;
+            }
 //            Log.d(TAG, "Image generator stopped.");
         }
     }
@@ -165,6 +178,7 @@ final class ImageGenerator {
                 } catch (IOException e) {
                     FirebaseCrash.report(e);
                 }
+                bitmap.recycle();
                 if (jpegByteArray != null) ApplicationContext.getJPEGQueue().add(jpegByteArray);
             }
         }, 500);
