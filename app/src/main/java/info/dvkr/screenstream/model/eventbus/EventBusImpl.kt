271a34372bc1fb8029c85ff8ce30861bcd207726
Dmitriy Krivoruchko <dkrivoruchko@gmail.com>
2017-09-05 09:53:37 +0300

    Support libs update

--- app/src/main/java/info/dvkr/screenstream/model/eventbus/EventBusImpl.kt
+++ app/src/main/java/info/dvkr/screenstream/model/eventbus/EventBusImpl.kt
@@ -7,10 +7,6 @@ import rx.subjects.PublishSubject
 class EventBusImpl : EventBus {
     private val events = PublishSubject.create<EventBus.GlobalEvent>()
 
-    init {
-        events.onBackpressureBuffer(32)
-    }
-
     override fun getEvent(): Observable<EventBus.GlobalEvent> = events.asObservable()
 
     override fun sendEvent(event: EventBus.GlobalEvent) = events.onNext(event)
