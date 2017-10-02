7d03a1fb64d8617d26153672c5b910db1457748d
Dmitriy Krivoruchko <dkrivoruchko@gmail.com>
2017-10-02 11:12:21 +0300

    Bug fix. Graphview: x-values has to be ordered in ASC

--- app/src/main/java/info/dvkr/screenstream/model/eventbus/EventBusImpl.kt
+++ app/src/main/java/info/dvkr/screenstream/model/eventbus/EventBusImpl.kt
@@ -4,14 +4,13 @@ import com.crashlytics.android.Crashlytics
 import com.jakewharton.rxrelay.PublishRelay
 import info.dvkr.screenstream.model.EventBus
 import rx.Observable
-import rx.functions.Action0
 
 class EventBusImpl : EventBus {
     private val TAG = "EventBusImpl"
     private val events = PublishRelay.create<EventBus.GlobalEvent>()
 
     override fun getEvent(): Observable<EventBus.GlobalEvent> = events.asObservable()
-            .onBackpressureBuffer(16, Action0 { Crashlytics.log(1, TAG, "onBackpressureBuffer: DROP") })
+            .onBackpressureBuffer(16, { Crashlytics.logException(IllegalStateException("onBackpressureBuffer: DROP")) })
 
     override fun sendEvent(event: EventBus.GlobalEvent) = events.call(event)
 }
