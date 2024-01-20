eb4667d333f17d786ec7b2a884f5f85aa6a2c955
Dmytro Kryvoruchko <dkrivoruchko@gmail.com>
2024-01-20 14:47:07 +0200

    4.0.31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "info.dvkr.screenstream"
         minSdk = 23
         targetSdk = 34
-        versionCode = 40030
-        versionName = "4.0.30"
+        versionCode = 40031
+        versionName = "4.0.31"
 
         // https://medium.com/@crafty/no-if-you-do-that-then-you-cant-use-newer-features-on-older-platforms-e-g-fa595333c0a4
         vectorDrawables.useSupportLibrary = true
