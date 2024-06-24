2971013f391364c77ad029b8014587b21bd74e41
Dmytro Kryvoruchko <dkrivoruchko@gmail.com>
2024-06-24 08:34:40 +0300

    Deps update

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,4 +19,4 @@ plugins {
 val minSdkVersion by extra(23)
 val targetSdkVersion by extra(34)
 val compileSdkVersion by extra(34)
-val buildToolsVersion by extra("34.0.0")
+val buildToolsVersion by extra("35.0.0")
